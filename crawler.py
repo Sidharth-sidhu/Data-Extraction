a3d8cfdcdc5ab6d4bdf623d2e912e00a4ef12ca3
sidharth <cosmictruce123@gmail.com>
2020-12-04 00:57:40 +0530

    Added new lines(json)

--- crawler.py
+++ crawler.py
@@ -10,7 +10,7 @@ class Baby:
         self.description = description
 
     def tojson(self):
-        return json.dump(self, default=lambda o: o.__dict__)
+        return json.dumps(self, default=lambda o: o.__dict__)
 
 
 chrome_path = r"driver/chromedriver"
@@ -23,6 +23,19 @@ try:
 finally:
     names = driver.find_element_by_class_name("names-list")
     items = names.find_elements_by_tag_name("li")
+
+
+data = {}
+data['babies'] = []
+
+for item in items:
+    babyNames = item.find_element_by_class_name("baby-name").text
+    babyNameMeanings = item.find_element_by_class_name("nm-ming").text
+    print(babyNames)
+    data['babies'].append(Baby(babyNames,babyNameMeanings).tojson())
+
+with open('babynames.json','w') as outfile:
+    json.dump(data,outfile)
 
 
 driver.close()
